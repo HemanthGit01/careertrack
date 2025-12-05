4ee805509f5c843435cf3f8edcc55c38a9013577
HemanthGit01 <hemanthveera8@gmail.com>
2025-12-05 12:40:23 +0530

    chore: initialize Spring Boot project

---
+++ careertrack-backend/careertrack-backend/src/test/java/com/hemanth/careertrack_backend/CareertrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.careertrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareertrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
